7d84bbacc901f416dfd3fcfd9ac47192103d64a8
Ronan Brett <Ronan.Brett@theaa.ie>
2017-08-05 16:57:09 +0100

    chore(scroll) Add Zenscroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MojsInit } from './shapes/mo-js.init';
 
+import * as zenscroll from 'zenscroll';
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
@@ -8,7 +10,13 @@ import { MojsInit } from './shapes/mo-js.init';
 })
 export class AppComponent {
 	title = 'app works!';
+
+	zen = zenscroll.createScroller(document.body, 750, 0);
 	constructor(mojo: MojsInit) {
 		mojo.init();
+	}
+	goToSection($event) {
+		const el = document.getElementById($event);
+		this.zen.to(el);
 	}
 }
